5c606b567e4d24fb662e04bb8f65c8b003fb4eb1
Randy Akerman <randy@akerman.org>
2022-12-07 10:06:23 -0500

    first run of pre-commit

--- ottoneu/lineup.py
+++ ottoneu/lineup.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import NoSuchElementException, WebDriverExceptio
 from selenium.webdriver.chrome.options import Options
 import pickle
 from constants import STATS
+
 # TODO: import fangraphs utils for login
 
 
@@ -16,7 +17,8 @@ def main():
     performance = scrape_fangraphs(fangraph_roster)
     # standings = get_current_standings()
     # lineup = optimize_lineup()
-    return(performance)
+    return performance
+
 
 def get_performance(driver, player_slug, player_id):
 
@@ -25,26 +27,33 @@ def get_performance(driver, player_slug, player_id):
     driver.get(url)
 
     try:
-        header = driver.find_element(By.XPATH, "//div[@id='daily-projections']/div[3]/div[@class='fg-data-grid undefined with-selected-rows sort-disabled  ']/div[@class='table-wrapper-outer']/div[@class='table-wrapper-inner']/div[@class='table-scroll']/table/thead")
+        header = driver.find_element(
+            By.XPATH,
+            "//div[@id='daily-projections']/div[3]/div[@class='fg-data-grid undefined with-selected-rows sort-disabled  ']/div[@class='table-wrapper-outer']/div[@class='table-wrapper-inner']/div[@class='table-scroll']/table/thead",
+        )
     except NoSuchElementException:
         return dict()
     except WebDriverException:
         return dict()
     else:
-        header_labels = header.find_elements(By.TAG_NAME, 'th')
+        header_labels = header.find_elements(By.TAG_NAME, "th")
 
     header_list = [th.text for th in header_labels]
 
-    body = driver.find_element(By.XPATH, "//div[@id='daily-projections']/div[3]/div[@class='fg-data-grid undefined with-selected-rows sort-disabled  ']/div[@class='table-wrapper-outer']/div[@class='table-wrapper-inner']/div[@class='table-scroll']/table/tbody")
-    body_values = body.find_elements(By.TAG_NAME, 'td')
+    body = driver.find_element(
+        By.XPATH,
+        "//div[@id='daily-projections']/div[3]/div[@class='fg-data-grid undefined with-selected-rows sort-disabled  ']/div[@class='table-wrapper-outer']/div[@class='table-wrapper-inner']/div[@class='table-scroll']/table/tbody",
+    )
+    body_values = body.find_elements(By.TAG_NAME, "td")
 
     body_list = [td.text for td in body_values]
-    
-    performance_dict = dict(zip(header_list,body_list))
-    player_info = {'player_id': player_id, 'player_slug':player_slug}
+
+    performance_dict = dict(zip(header_list, body_list))
+    player_info = {"player_id": player_id, "player_slug": player_slug}
     performance_dict.update(player_info)
 
-    return(performance_dict)
+    return performance_dict
+
 
 def scrape_fangraphs(fangraph_roster):
     service = Service(executable_path=os.getenv("CHROME_PATH"))
@@ -54,11 +63,18 @@ def scrape_fangraphs(fangraph_roster):
 
     login(driver)
 
-    performance_dict = [get_performance(driver,y,x) for x,y in zip(fangraph_roster["FG MajorLeagueID"],fangraph_roster["name_slug"])]
+    performance_dict = [
+        get_performance(driver, y, x)
+        for x, y in zip(
+            fangraph_roster["FG MajorLeagueID"], fangraph_roster["name_slug"]
+        )
+    ]
 
-    filtered_dict = list(filter(None,performance_dict))
+    filtered_dict = list(filter(None, performance_dict))
     # TODO: Split between pitchers and batters
-    filtered_stats = [{key : val for key, val in sub.items() if key in STATS} for sub in filtered_dict]
+    filtered_stats = [
+        {key: val for key, val in sub.items() if key in STATS} for sub in filtered_dict
+    ]
     # Hitters: PA	R	HR	OBP	SLG available (R,HR)
     # TODO: Develop OBP and SLG proxy
     # OBP = (Hits + Walks + Hit by Pitch) / (At Bats + Walks + Hit by Pitch + Sacrifice Flies)
@@ -73,16 +89,17 @@ def scrape_fangraphs(fangraph_roster):
     pitchers = []
 
     for i in range(len(filtered_stats)):
-        if 'Pos' in filtered_stats[i].keys():
+        if "Pos" in filtered_stats[i].keys():
             batters.append(filtered_stats[i])
         else:
             pitchers.append(filtered_stats[i])
 
     performance = dict()
-    performance['batters'] = batters
-    performance['pitchers'] = pitchers
-    pickle.dump(performance, open('tmp/performance.pickle', 'wb'))
-    return(performance)
+    performance["batters"] = batters
+    performance["pitchers"] = pitchers
+    pickle.dump(performance, open("tmp/performance.pickle", "wb"))
+    return performance
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- ottoneu/reports.py
+++ ottoneu/reports.py
@@ -4,8 +4,9 @@ import os
 from slugify import slugify
 from valuation import valuation
 
+
 def main():
-    report_generator(report_type='cut_or_keep')
+    report_generator(report_type="cut_or_keep")
 
 
 # def daily_roster_report(roster):
@@ -33,18 +34,23 @@ def main():
 #     output_file.write(rendered_file)
 #     output_file.close()
 
+
 def report_generator(report_type):
-    if report_type == 'cut_or_keep':
+    if report_type == "cut_or_keep":
         data = valuation()
 
-     
-    with open(os.path.join(os.getenv("HUGO_TEMPLATES"),f'{report_type}.md'), 'r') as file:
+    with open(
+        os.path.join(os.getenv("HUGO_TEMPLATES"), f"{report_type}.md"), "r"
+    ) as file:
         template = Template(file.read(), trim_blocks=True)
     rendered_file = template.render(data=data)
 
     # TODO: Delete file if already exists
-    file_name = os.path.join(os.getenv("HUGO_CONTENT_PATH"),f"{slugify(report_type)}-{slugify(data['team_name'])}-{data['date']}.md")
-    
+    file_name = os.path.join(
+        os.getenv("HUGO_CONTENT_PATH"),
+        f"{slugify(report_type)}-{slugify(data['team_name'])}-{data['date']}.md",
+    )
+
     if os.path.exists(file_name):
         os.remove(file_name)
 
@@ -53,5 +59,6 @@ def report_generator(report_type):
     output_file.write(rendered_file)
     output_file.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
